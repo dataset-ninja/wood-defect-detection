695409301459371ae250a5843f84518cd626313e
Ars Vladimirov <grokhael@gmail.com>
2023-06-23 22:00:22 +0400

    import unpack_if_archive from dtools

--- src/convert.py
+++ src/convert.py
@@ -8,19 +8,12 @@ from urllib.parse import unquote, urlparse
 import numpy as np
 import supervisely as sly
 from cv2 import connectedComponents
-
-# from dataset_tools.convert import unpack_if_archive
 from dotenv import load_dotenv
 from supervisely.imaging.color import hex2rgb
-from supervisely.io.fs import (
-    dir_exists,
-    file_exists,
-    get_file_ext,
-    get_file_name,
-    get_file_name_with_ext,
-)
+from supervisely.io.fs import dir_exists, file_exists, get_file_ext, get_file_name
 
 import src.settings as s
+from dataset_tools.convert import unpack_if_archive
 
 # if sly.is_development():
 # load_dotenv("local.env")
@@ -46,49 +39,6 @@ segm_suffix = "_segm"
 bbox_suffix = "_anno.txt"
 
 import os
-import tarfile
-import zipfile
-
-from tqdm import tqdm
-
-
-def unpack_if_archive(path: str) -> str:
-    if os.path.isdir(path):
-        return path
-
-    extraction_path = os.path.splitext(path)[0]
-
-    if zipfile.is_zipfile(path):
-        os.makedirs(extraction_path, exist_ok=True)
-
-        with zipfile.ZipFile(path, "r") as zip_ref:
-            total_files = len(zip_ref.infolist())
-
-            with tqdm(
-                desc=f"Unpacking {get_file_name_with_ext(path)}...", total=total_files, unit="file"
-            ) as pbar:
-                for file in zip_ref.infolist():
-                    zip_ref.extract(file, extraction_path)
-                    pbar.update(1)
-
-            return extraction_path
-
-    if tarfile.is_tarfile(path):
-        os.makedirs(extraction_path, exist_ok=True)
-
-        with tarfile.open(path, "r") as tar_ref:
-            total_files = len(tar_ref.getnames())
-
-            with tqdm(
-                desc=f"Unpacking {get_file_name_with_ext(path)}...", total=total_files, unit="file"
-            ) as pbar:
-                for file in tar_ref.getnames():
-                    tar_ref.extract(file, extraction_path)
-                    pbar.update(1)
-
-            return extraction_path
-
-    return path
 
 
 def download_dataset(teamfiles_dir: str):
