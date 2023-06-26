5647cbc0e8046c3f02674ff7a2d429664b649773
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-06-26 14:57:09 +0600

    Update options.py

--- src/options.py
+++ src/options.py
@@ -18,8 +18,8 @@ POSTER_TITLE: str = None
 
 ###############################################################
 #### * Set up visualization params for HorizontalGrid class ###
-HORIZONTAL_GRID_ROWS: int = None
-HORIZONTAL_GRID_COLS: int = None
+HORIZONTAL_GRID_ROWS: int = 2
+HORIZONTAL_GRID_COLS: int = 4
 HORIZONTAL_GRID_IS_DETECTION_TASK: bool = IS_DETECTION_TASK
 ###############################################################
 
