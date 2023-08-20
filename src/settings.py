ab8145452b87893cccc2de08eaad518648a78b79
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-20 21:45:55 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -45,7 +45,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://zenodo.org/record/4694695#.YkWqTX9Bzmg"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 845345
+PREVIEW_IMAGE_ID: int = 2085949
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/wood-defect-detection"
